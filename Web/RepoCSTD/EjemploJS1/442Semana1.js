5db82e9b33f5f9f61ea6bc16eede493682b401c9
a01660761 <a01660761@tec.mx>
2026-02-25 13:37:14 -0600

    Update 25.2.2026

---
+++ Web/RepoCSTD/EjemploJS1/442Semana1.js
@@ -0,0 +1,10 @@
+function texto() {
+    document.getElementsByTagName("img")[0].src = "https://upload.wikimedia.org/wikipedia/commons/9/9a/Super_Stationmaster_Tama_20090104.JPG";
+}
+
+function ciclo() {
+    for (let i = 0; i < 10; i++) {
+        document.getElementById("p2").innerHTML = i;
+
+    }
+}
