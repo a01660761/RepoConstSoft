0b07b1d6638438e9926c04b6805e7aff4feb53d3
a01660761 <a01660761@tec.mx>
2026-02-25 22:19:31 -0600

    Update First Web Page P1 - 25.2.2026

--- Web/RepoCSTD/PrimeraPagina/index.js
+++
@@ -1,7 +0,0 @@
-function traducir() {
-    document.getElementById("par1").innerHTML="Traduccion";
-}
-
-function cambiarFondo() {
-    document.getElementById("Body")[0].style.backgroundColor="blue";
-}
---
+++ Web/RepoClaseDemos/PrimeraPagina/index.js
@@ -0,0 +1,7 @@
+function traducir() {
+    document.getElementById("par1").innerHTML = "Traduccion";
+}
+
+function cambiarFondo() {
+    document.getElementsByTagName("Body")[0].style.backgroundColor = "blue";
+}
