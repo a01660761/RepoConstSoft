2600021c9cf48522d99a5330764877effc3c6cfd
a01660761 <a01660761@tec.mx>
2026-03-24 14:38:16 -0600

    Calculator + API

--- Web/MYAPP/app.js
+++ Web/MYAPP/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3005
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -14,15 +17,17 @@ app.get('/variable/:nombre', (req, res) => {
     let x = req.params.nombre;
     let saludo = "hola ";
     x = saludo + x;
-    res.send(x)
+    res.send('hola')
 })
 
-app.get('/suma', (req, res) => {
-    let x = parseFloat(req.params.var1);
-    let y = parseFloat(req.params.var2);
+app.post('/suma', (req, res) => {
+    let x = parseFloat(req.primero);
+    let y = parseFloat(req.segundo);
     resultado = x + y;
     res.send(resultado)
 })
+
+app.use('coldPic', express.static('ice-man.jpg'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
